f8ac31d4fbc695bdcb576c406b486ed7d74418c6
OldDog-Z124 <OldDog.Z124@qq.com>
2023-03-04 15:29:29 +0800

    add step7

--- src/App.js
+++ src/App.js
@@ -79,7 +79,11 @@ const App = () => {
         number: newNumber,
       }
   
-      setPersons(persons.concat(personObject))
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          setPersons(persons.concat(response.data))
+        })
     }
 
     setNewName('')
